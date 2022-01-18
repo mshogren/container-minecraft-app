34c04acd870333ec47424a00a8d0f56b5f4dc60e
mshogren <3902009+mshogren@users.noreply.github.com>
2022-01-18 11:23:37 -0700

    feat(app): basic navigation and layout

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,54 @@
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import './App.css';
+
+function Layout() {
+  return (
+    <div>
+      <nav className="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+        <ul className="pure-menu-list">
+          <li className="pure-menu-item">
+            <Link className="pure-menu-link" to="/">
+              Home
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <main className="content-wrapper">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="splash-container">
+      <div className="splash">
+        <h1 className="splash-head">Container Minecraft App</h1>
+        <p className="splash-subhead">
+          A containerized app that provides a nifty interface for managing your
+          containerized Minecraft servers.
+        </p>
+
+        <p>Nothing here</p>
+      </div>
+    </div>
+  );
+}
+
 function App() {
-  return <span>Nothing here</span>;
+  return (
+    <div>
+      <h1>Container Minecraft App</h1>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import 'purecss/build/pure-min.css';
+import 'purecss/build/grids-responsive-min.css';
 import App from './App';
 
 ReactDOM.render(
