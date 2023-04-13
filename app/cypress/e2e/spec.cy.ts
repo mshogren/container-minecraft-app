bd6086426a8750dc901416dc8a64e7f17340401f
mshogren <3902009+mshogren@users.noreply.github.com>
2023-04-12 20:39:31 -0600

    test: add e2e tests

---
+++ app/cypress/e2e/spec.cy.ts
@@ -0,0 +1,69 @@
+describe('splash screen', () => {
+  it('opens', () => {
+    cy.visit('/');
+  });
+});
+
+describe('servers list', () => {
+  it('opens', () => {
+    cy.visit('/servers');
+  });
+});
+
+describe('server add', () => {
+  it('opens', () => {
+    cy.visit('/servers/add');
+  });
+
+  it('adds a vanilla server', () => {
+    const name = 'Cypress-Vanilla-Test';
+    cy.visit('/servers/add');
+
+    cy.get('select').select('vanilla');
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/servers/add/vanilla');
+    });
+
+    cy.get('input').first().type(name);
+    cy.get('div .input-container option').first().click();
+    cy.get('button').contains('Add').click();
+
+    cy.get('button', { timeout: 30000 }).contains('OK').click();
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/servers');
+    });
+
+    cy.get('tr').contains(name).get('a').contains('Details').click();
+    cy.get('button').contains('Stop').click();
+    cy.contains('exited', { timeout: 30000 });
+
+    cy.exec(`docker rm ${name}`);
+    cy.exec(`docker volume rm ${name}`);
+  });
+
+  it('adds a curseforge server', () => {
+    const name = 'Cypress-Curse-Test';
+    cy.visit('/servers/add');
+
+    cy.get('select').select('curse');
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/servers/add/curse');
+    });
+
+    cy.get('input').first().type(name);
+    cy.get('div .input-container option').first().click();
+    cy.get('button').contains('Add').click();
+
+    cy.get('button', { timeout: 30000 }).contains('OK').click();
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq('/servers');
+    });
+
+    cy.get('tr').contains(name).get('a').contains('Details').click();
+    cy.get('button').contains('Stop').click();
+    cy.contains('exited', { timeout: 30000 });
+
+    cy.exec(`docker rm ${name}`);
+    cy.exec(`docker volume rm ${name}`);
+  });
+});
